1ab1a192b5f4b7845b509411ae6dec34b4e69e8e
Obi <obinnawalker@gmail.com>
2017-11-05 23:25:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = {}
+  var variable = array
+
+  for(var i=0; i<musicians.length; i++){
+
+    array[i]=[`${musicians[i]} plays ${instruments[i]}`];
+  }
+  return array
+}
